90961ef990d57bf49f7430386c81ad2cf46d94e3
Szymon Samus <siizmonsus@gmail.com>
2024-03-26 14:50:20 +0100

    Initital Commit: Added a header section.

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div id="header">
+        <img src="./investment-calculator-logo.png" alt="Cartoon Image of a Money Bag"/>
+        <h1 id="">Investment Calculator</h1>
+    </div>
+  )
+}
