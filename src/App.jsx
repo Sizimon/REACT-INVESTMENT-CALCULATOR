1d41e35cfa5188cfeecf08c861f8b52bf6b221c7
Szymon Samus <siizmonsus@gmail.com>
2024-03-26 16:30:40 +0100

    Added: State functionality to set the investment fields to an initial null value, and update them when the user types in their desired values.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import UserInput from './components/UserInput'
 import Results from './components/Results'
 
+
 function App() {
   return (
   <>
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,71 @@
+import { useState } from 'react';
+
 export default function UserInput() {
+    const [userInput, setUserInput] = useState({
+        initialInvestment: null,
+        annualInvestment: null,
+        expectedReturn: null,
+        duration: null
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue
+            };
+
+        });
+    }
+
     return (
         <form id="user-input">
             <div className="input-group">
                 <span>
                     <label>Initial Investment</label>
-                    <input type="number" required />
+                    <input 
+                        type="number"
+                        required
+                        value={userInput.initialInvestment} 
+                        onChange={(event) => 
+                            handleChange('initialInvestment', event.target.value)
+                        } 
+                    />
                 </span>
                 <span>
                     <label>Annual Investment</label>
-                    <input type="number" required />
+                    <input
+                        type="number"
+                        required
+                        value={userInput.annualInvestment}
+                        onChange={(event) => 
+                            handleChange('annualInvestment', event.target.value)
+                        }  
+                    />
                 </span>
             </div>
             <div className="input-group">
                 <span>
                     <label>Expected Return</label>
-                    <input type="number" required />
+                    <input
+                        type="number"
+                        required
+                        value={userInput.expectedReturn}
+                        onChange={(event) => 
+                            handleChange('expectedReturn', event.target.value)
+                        }  
+                    />
                 </span>
                 <span>
                     <label>Duration</label>
-                    <input type="number" required />
+                    <input
+                        type="number"
+                        required 
+                        value={userInput.duration}
+                        onChange={(event) => 
+                            handleChange('duration', event.target.value)
+                        }  
+                    />
                 </span>
             </div>
         </form>
